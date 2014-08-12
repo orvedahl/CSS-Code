047c5ab81adae72deb1b82c926f50f1c245cb9ca
Ryan Orvedahl <ryan.orvedahl@colorado.com>
2014-08-12 14:15:01 -0600

    return radius as well as rossby for plotting purposes

--- CSS-Python/rossby_from_checkpoint.py
+++ CSS-Python/rossby_from_checkpoint.py
@@ -8,17 +8,17 @@
 import numpy
 import vectorcurl
 
-def rossby_from_chk(iter, case, omega0):
+def rossby_from_chk(iter, case, omega0, return_rad=False):
 
     # get curl
-    curl, rad, theta, phi = vectorcurl.vectorcurl(iter, case)
+    curl, radius, theta, phi = vectorcurl.vectorcurl(iter, case)
 
     # get magnitude of curl --> sqrt(enstrophy)
     ens = numpy.sqrt(curl[:,:,:,0]*curl[:,:,:,0] +
                      curl[:,:,:,1]*curl[:,:,:,1] +
                      curl[:,:,:,2]*curl[:,:,:,2])
 
-    nr = len(rad)
+    nr = len(radius)
     rossby = numpy.zeros((nr))
     for ir in range(nr):
         rossby[ir] = numpy.average(ens[:,:,ir])
@@ -26,6 +26,9 @@ def rossby_from_chk(iter, case, omega0):
     # divide by rotation rate of the frame
     rossby[:] = rossby[:]/omega0
 
-    return rossby
+    if (return_rad):
+        return rossby, radius
+    else:
+        return rossby
 
 
