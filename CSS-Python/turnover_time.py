76393f2dea726a4f09f41ea581d06e1f457bee59
Ryan Orvedahl <ryan.orvedahl@colorado.com>
2014-08-22 15:31:43 -0600

    try more options for turnover times

--- CSS-Python/turnover_time.py
+++ CSS-Python/turnover_time.py
@@ -10,6 +10,8 @@ import getopt
 import numpy
 from read_checkpoint import *
 
+import pylab
+
 def turnover_time(iter, case):
 
     # option 1:
@@ -56,11 +58,20 @@ def turnover_time(iter, case):
     # calculate the RMS velocity
     vr_rms = rms(vr)
 
+    # rms of 1./vr
+    inv_vr = 1./vr
+    ind_0 = numpy.where(vr==0.)
+    inv_vr[ind_0] = 0.
+    vrinv_rms = rms(inv_vr)
+
     # find up-flows and down-flows
     vr_up   = vr[vr > 0]
     vr_down = vr[vr < 0] 
 
-    vr_rms_u = rms(vr_up)
+    if (len(vr_up) > 0):
+        vr_rms_u = rms(vr_up)
+    else:
+        vr_rms_u = 0.
     vr_rms_d = rms(vr_down)
 
     tau_1 = 2. * radial_domain / vr_rms
@@ -83,13 +94,94 @@ def turnover_time(iter, case):
     print "\tTau (days): "+str(tau_2/86400.)
 
     # midpoint rule (trap rule has issues with 1/0)
-    tau_3 = radial_domain * abs(1./vr[nr/2])
+    tau_3 = integrate(abs(vr), radius, method='simp')
+    # tau_3 = radial_domain * abs(1./vr[nr/2])
 
     print "\nOption 3:"
     print "\tuse integral"
     print "\tTau (sec) : "+str(tau_3)
     print "\tTau (days): "+str(tau_3/86400.)
+
+    tau_4 = 2. * radial_domain * vrinv_rms
+
+    print "\nOption 4:"
+    print "\tuse RMS of 1/Vr"
+    print "\tTau (sec) : "+str(tau_4)
+    print "\tTau (days): "+str(tau_4/86400.)
+
+    frac_u = 0.8
+    frac_d = 0.05
+    rlow = radius[0] + frac_d*radial_domain
+    rhi = radius[0] + frac_u*radial_domain
+    rad_inner = numpy.where((rlow < radius) & (radius < rhi))
+    vr_restrict = vr[rad_inner]
+    vr_restrict_rms = rms(vr_restrict)
+
+    # restricted domain = rhi - rlow
+    tau_5 = 2.*(rhi - rlow) / vr_restrict_rms
+
+    print "\nOption 5:"
+    print "\tuse RMS of restricted Vr"
+    print "\tlower bdry: "+str(frac_d)
+    print "\tupper bdry: "+str(frac_u)
+    print "\tTau (sec) : "+str(tau_5)
+    print "\tTau (days): "+str(tau_5/86400.)
+
+    tau_6 = 2.*radial_domain / numpy.amax(abs(vr))
+
+    print "\nOption 6:"
+    print "\tuse abs(max(Vr))"
+    print "\tTau (sec) : "+str(tau_6)
+    print "\tTau (days): "+str(tau_6/86400.)
     print
+
+
+def integrate(vr, rad, method='simp'):
+
+    intgrnd = 1./vr
+
+    # deal with possible 1./0.
+    ind_0 = numpy.where(vr == 0.)
+    intgrnd[ind_0] = 0.
+
+    integral = 0.0
+
+    # simpson's method with even/odd number of slabs
+    if (method == "simp"):
+
+        nslabs = len(rad) - 1
+
+        if nslabs%2==0:
+           M = nslabs
+           odd = False
+        else:
+           M = nslabs - 1
+           odd = True
+
+        # assumes a uniform grid
+        delta = rad[1] - rad[0]
+
+        n = 0
+        while (n < M):
+            # simpson integration over even number of slabs
+            integral += (1./3.)*delta*(\
+                                 intgrnd[n] + 4.*intgrnd[n+1] + intgrnd[n+2])
+            n += 2
+
+        if (odd):
+            # include last slab if it exists
+            integral += delta/12.*(\
+                -intgrnd[nslabs-2] + 8.*intgrnd[nslabs-1] + 5.*intgrnd[nslabs])
+
+    # trapezoid rule
+    elif (method == "trap"):
+        n = 0
+        while (n < len(rad)-1):
+            #            <----width---->  <------height in middle------>
+            integral += (rad[n+1]-rad[n])*0.5*(intgrnd[n] + intgrnd[n+1])
+            n += 1
+
+    return integral
 
 
 def rms(x, axis=None):
