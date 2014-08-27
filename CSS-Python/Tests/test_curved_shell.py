3ae05cbdbfa018af64686d11d4d860ca5bf01269
Ryan Orvedahl <ryan.orvedahl@colorado.com>
2014-08-27 14:57:55 -0600

    curved shell and test code seem to be working now

--- CSS-Python/Tests/test_curved_shell.py
+++ CSS-Python/Tests/test_curved_shell.py
@@ -1,21 +1,54 @@
 
+import sys
+import getopt
 import pylab
 import numpy
 from curved_shell import *
 
+err = False
+proj = 'ortho'
+small = False
 plot_original = False
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "p:os", ["proj","orig","small"])
+except getopt.GetoptError:
+    print "\n---ERROR: Unknown Command Line Option---\n"
+    err = True
 
-phi   = numpy.arange(-40, 40, 0.1)
-theta = numpy.arange(50., 130., 0.1)
-theta = theta*numpy.pi/180.
-phi   = phi*numpy.pi/180.
+if (not err):
+    for opt, arg in opts:
+        if opt in ("-p", "--proj"):
+           proj = arg
+        elif opt in ("-s", "--small"):
+           small = True
+        elif opt in ("-o", "--orig"):
+           plot_original = True
+
+if small:
+   phi_d   = numpy.arange(-10, 10.+0.1, 0.1)
+   theta_d = numpy.arange(80., 100.+0.1, 0.1)
+else:
+   phi_d   = numpy.arange(-40, 40.+0.5, 0.5)
+   theta_d = numpy.arange(50., 130.+0.5, 0.5)
+
+theta = theta_d*numpy.pi/180.
+phi   = phi_d*numpy.pi/180.
 
 a = numpy.zeros((len(theta), len(phi)))
 for i in range(len(theta)):
     th = theta[i]
     for j in range(len(phi)):
         p = phi[j]
-        a[i,j] = numpy.sin(th)*numpy.cos(p)
+
+        # should be positive in top-right & bottom-left corners
+        # and negative in top-left & bottom-right corners and zero elsewhere
+        # (this assumes theta includes 90 deg and phi includes 0 deg)
+        #a[i,j] = numpy.cos(th)*numpy.sin(p)
+
+        # should be bulls-eye with peak of 1.0 centered on th=90, phi=0
+        # should also be elongated with largest edge in theta direction
+        # (this assumes theta includes 90 deg and phi includes 0 deg)
+        a[i,j] = numpy.sin(th)*numpy.cos(2.*p)
 
 fig = pylab.figure(figsize=(7,7), dpi=100)
 
@@ -31,22 +64,34 @@ extent=None
 if (plot_original):
     axes = fig.add_subplot(111)
 
+    extent=(numpy.min(phi_d), numpy.max(phi_d), 
+            numpy.max(theta_d), numpy.min(theta_d))
+
     im = axes.imshow(a, aspect=aspect, cmap=cmap, origin="upper", 
                       extent=extent, interpolation='quadric', 
                       vmin=vmin, vmax=vmax)
     cb = fig.colorbar(im)
-    cb.set_clim(vmin, vmac)
+    cb.set_clim(vmin, vmax)
     cb.set_label=cb_title
 
     pylab.title("Hello")
+    pylab.xlabel("phi")
+    pylab.ylabel("theta")
     pylab.show()
 
 else:
-
-    curved_image(fig, a, equator=False, vmin=vmin, vmax=vmax,
+    nphi=len(phi)
+    nth = len(theta)
+    extent = [phi[nphi/4], phi[3*nphi/4], 
+              0.5*numpy.pi-theta[nth/4], 0.5*numpy.pi - theta[3*nth/4]]
+    extent = None
+    lat0 = 0.5*numpy.pi - theta[len(theta)/2]
+    lon0 = phi[len(phi)/2]
+    curved_image(a, phi, theta, radians=True, grid=True, 
+                 vmin=vmin, vmax=vmax,
                  cmap=cmap, cbar=True, cb_title=cb_title, 
-                 aspect=aspect, extent=extent, proj='aitoff')
-    pylab.title("Hello")
+                 extent=extent, proj=proj, lat_0=lat0, lon_0=lon0, 
+                 title='Hello', xlabel='long', ylabel='lat')
 
     pylab.show()
 
--- CSS-Python/curved_shell.py
+++ CSS-Python/curved_shell.py
@@ -8,26 +8,53 @@
 
 import numpy
 import pylab
+#from convert_from_lat_lon import *
+from mpl_toolkits.basemap import Basemap
 
-# CURRENT pyASH VERSION USES matplotlib.basemap.Basemap
+# CURRENT pyASH VERSION USES mpl_toolkits.basemap.Basemap
 
 #####################################################################
 # plot curved 2D colormap
 #####################################################################
-def curved_image(fig, data, equator=True, vmin=None, 
-                 vmax=None, cmap="jet", cbar=True, aspect=1, extent=None, 
-                 cb_title='', proj='mollweide'):
+def curved_image(data, phi, theta, radians=True, grid=True, vmin=None, 
+                 vmax=None, cmap="jet", cbar=True, extent=None, 
+                 cb_title='', proj='ortho', title='', xlabel='Longitude (deg)',
+                 ylabel='Latitude (deg)', lat_0=0, lon_0=0, nlon=5, nlat=5):
 
-    # fig                : matplotlib figure instance on which to plot data
     # data               : 2D array of size (ntheta, nphi)
-    # equator            : plot dotted line at equator
+    # phi                : 1D array of phi values
+    # theta              : 1D array of theta values
+    # lon_0              : central longitude (phi direction)
+    # lat_0              : central latitude (theta direction)
+    # radians            : are phi, theta, lat_0 & lon_0 in radians or degrees
+    # grid               : plot latitude/longitude grid lines
     # vmin, vmax         : min, max colorbar values
     # cmap               : matplotlib colormap instance to use
     # cbar               : include a colorbar
-    # extent             : (rmin, rmax, theta_min, theta_max)
+    # extent             : (phi_min, phi_max, theta_min, theta_max)
     # cb_title           : title for colorbar
-    # proj               : projection to use ('mollweide', 'aitoff', 
-    #                       'hammer', 'lamber', 'polar', 'rectilinear')
+    # nlon               : number of longitude lines to draw
+    # nlat               : number of latitude lines to draw
+    # proj               : projection to use ('moll', 'tmerc', 'merc', 'omerc'
+    #                        'robin', 'ortho', ...
+    # title              : give a title to the plot
+    # xlabel             : give a label to the x-axis
+    # ylabel             : give a label to the y-axis
+
+    # convert everything to degrees
+    if (radians):
+        theta = theta * 180. / numpy.pi
+        phi = phi * 180. / numpy.pi
+        lat_0 = lat_0 * 180. / numpy.pi
+        lon_0 = lon_0 * 180. / numpy.pi
+        if (extent != None):
+            extent[0] = extent[0] * 180. / numpy.pi # longitude min
+            extent[1] = extent[1] * 180. / numpy.pi # longitude max
+            extent[2] = extent[2] * 180. / numpy.pi # latitude min
+            extent[3] = extent[3] * 180. / numpy.pi # latitude max
+
+    nphi = len(phi)
+    nth  = len(theta)
 
     # set defaults
     if (vmin == None):
@@ -35,35 +62,106 @@ def curved_image(fig, data, equator=True, vmin=None,
     if (vmax == None):
         vmax = numpy.amax(data)
 
-    # set projection
-    ax = fig.add_subplot(1,1,1, projection=proj)
-
-    # plot equator
-    if (equator):
-        pylab.plot([r[0],r[-1]], [0,0], 'k--')
-
+    # set plot extent
     if (extent == None):
-        extent = None
-        #fact = 0.02
-        #pylab.xlim(xmin-fact*(xmax-xmin), xmax+fact*(xmax-xmin))
-        #pylab.ylim(ymin-fact*(ymax-ymin), ymax+fact*(ymax-ymin))
-    else:
-        pylab.xlim(extent[0], extent[1])
-        if (extent[2] > extent[3]):
-            pylab.ylim(extent[3], extent[2])
-        else:
-            pylab.ylim(extent[2], extent[3])
+        lonmin = phi[0]
+        lonmax = phi[-1]
+        latmin = 90. - theta[-1]
+        latmax = 90. - theta[0]
 
-    # use imshow
-    im = ax.imshow(data, vmin=vmin, vmax=vmax, extent=extent, cmap=cmap,
-                      origin='upper', clip_on=False, aspect=aspect, 
-                      interpolation='quadric')
+    else:
+        lonmin = extent[0]
+        lonmax = extent[1]
+        latmin = extent[2]
+        latmax = extent[3]
+
+    fact = 0.0
+    ll_lon = lonmin - fact*(lonmax - lonmin) # lon of lower left corner
+    ll_lat = latmin - fact*(latmax - latmin) # lat of lower left corner
+    ur_lon = lonmax + fact*(lonmax - lonmin) # lon of upper right corner
+    ur_lat = latmax + fact*(latmax - latmin) # lat of upper right corner
+
+    # set up main map
+    map = Basemap(projection=proj, lon_0=lon_0, lat_0=lat_0)
+
+    # set boundaries of map
+    ll_x, temp = map(ll_lon, lat_0)  # lat_0 because it is the widest
+    temp, ll_y = map(lon_0, ll_lat)  # lon_0 because it is the widest
+    ur_x, temp = map(ur_lon, lat_0)  # lat_0 because it is the widest
+    temp, ur_y = map(lon_0, ur_lat)  # lon_0 because it is the widest
+
+    map.llcrnrx = ll_x
+    map.llcrnry = ll_y
+    map.urcrnrx = ur_x
+    map.urcrnry = ur_y
+
+    # set up grid
+    lon_tmp = phi
+    lat_tmp = 90. - theta
+    x, y = map(*numpy.meshgrid(lon_tmp, lat_tmp))
+
+    # STANDARD pcolormesh:
+    # if data is data(x,y) then calling sequence is:
+    #       pcolormesh(y, x, data, ...)
+    # or
+    #       pcolormesh(x, y, data.transpose(), ...)
+    # data array has dimensions (nth, nphi)
+    #
+    # MAP.pcolormesh:
+    # if data is data(x,y) then calling sequence is:
+    #       pcolormesh(x, y, data, ...)
+    im = map.pcolormesh(x, y, data, cmap=cmap, latlon=True, 
+                        vmin=vmin, vmax=vmax)
+    #map.drawmapboundary()
+
+    # plotting grid lines
+    if (grid):
+        dlon = (lonmax-lonmin)/(nlon+1)
+        dlat = (latmax-latmin)/(nlat+1)
+        parallels = numpy.arange(lonmin, lonmax+dlon, dlon)
+        meridians = numpy.arange(latmin, latmax+dlat, dlat)
+        col = "0.1"
+        ls = ":"
+        for par in parallels:
+            xl, yl = map(lonmin, par)
+            xr, yr = map(lonmax, par)
+
+            # line itself
+            map.plot([xl,xr], [yl,yr], linestyle=ls, color=col)
+
+            # label
+            titl = str(round(par,1))
+            x, y = map(lonmin-0.10*(lonmax-lonmin), par)
+            pylab.text(x, y, titl, fontsize=7)
+
+        for mer in meridians:
+            latl, lonl = latmin, mer
+            latr, lonr = latmax, mer
+
+            # line itself
+            map.drawgreatcircle(lonl,latl,lonr,latr,linestyle=ls,color=col)
+
+            # label
+            titl = str(round(mer,1))
+            x, y = map(mer, latmin-0.03*(latmax-latmin))
+            pylab.text(x, y, titl, fontsize=7)
+
+        # OLD WAY:
+        # labels=[left, right, top, bottom]
+        # only label latitude lines that intersect the left
+        # only label longitude lines that intersect the bottom
+        #map.drawparallels(parallels, latmax=latmax, labels=[1,0,0,0])
+        #map.drawmeridians(meridians, latmax=latmax, labels=[0,0,0,1])
 
     # plot colorbar
     if (cbar):
-        cb = pylab.colorbar(im)
+        cb = pylab.colorbar(im, shrink=0.9, pad=0.05)
         cb.set_clim(vmin, vmax)
         cb.set_label(cb_title)
+
+    pylab.title(title)
+    pylab.xlabel(xlabel, labelpad=18)
+    pylab.ylabel(ylabel, labelpad=33)
 
     return
 
